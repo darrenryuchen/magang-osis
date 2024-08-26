59e01fb856a2e29eee8193f02a755aa5273f1a44
Instencial <93496811+Instencialer@users.noreply.github.com>
2024-08-26 21:02:10 +0700

    Update script.js

--- magang/script.js
+++ magang/script.js
@@ -4,24 +4,24 @@ window.addEventListener('load', () => {
       delay: 400
     });
     animateslide(); 
-  });
-  
-  function animateslide() {
-    const slide = document.getElementById('slidewindow');
-    if (slide) {
-      slide.classList.add('slide');
-      setTimeout(() => {
-        slide.classList.remove('slide');
-      }, 2000);
-    }
-  }
-  
-  const hamburger = document.getElementById('hamburger');
-  const navbar = document.querySelector('.navbar');
-  const navUl = navbar.querySelector('ul');
-  
-  hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-    navUl.classList.toggle('animate');
-    navUl.classList.toggle('closing');
-  });
+
+    function animateslide() {
+        const slide = document.getElementById('slidewindow');
+        if (slide) {
+            slide.classList.add('slide');
+            setTimeout(() => {
+            slide.classList.remove('slide');
+            }, 2000);
+        }
+        }
+
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.querySelector('.navbar');
+    const navUl = navbar.querySelector('ul');
+
+    hamburger.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+        navUl.classList.toggle('animate');
+        navUl.classList.toggle('closing');
+    });
+});
