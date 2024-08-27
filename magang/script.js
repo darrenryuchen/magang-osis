5e95d20c86f0b49ee118ccf055621cf76a31adb0
Instencial <93496811+Instencialer@users.noreply.github.com>
2024-08-27 19:17:35 +0700

    Update script.js

--- magang/script.js
+++ magang/script.js
@@ -19,7 +19,7 @@ window.addEventListener('load', () => {
     const navbar = document.querySelector('.navbar');
     const navUl = navbar.querySelector('ul');
 
-    hamburger.addEventListener('touchstart', () => {
+    hamburger.addEventListener('pointerup', () => {
         navbar.classList.toggle('active');
         navUl.classList.toggle('animate');
         navUl.classList.toggle('closing');
