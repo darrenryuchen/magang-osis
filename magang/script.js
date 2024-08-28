0dc7a62d349e1fea5ef5bd5070d7340bbfc2a554
Instencial <93496811+Instencialer@users.noreply.github.com>
2024-08-28 12:23:35 +0700

    Update script.js

--- magang/script.js
+++ magang/script.js
@@ -23,5 +23,66 @@ window.addEventListener('load', () => {
         navbar.classList.toggle('active');
         navUl.classList.toggle('animate');
         navUl.classList.toggle('closing');
-    });
+    })
+
+    document.querySelectorAll(".carousel").forEach(carousel => {
+        const items = carousel.querySelectorAll(".carousel__item");
+        const buttonsHtml = Array.from(items, () => {
+            return '<span class="carousel__button"></span>';
+        });
+
+        carousel.insertAdjacentHTML("beforeend", `
+            <div class="carousel__nav">
+                ${buttonsHtml.join("")}
+            </div>
+        `);
+
+        let maks = items.length-1;
+        let idx = 0;
+
+        const buttons = carousel.querySelectorAll(".carousel__button");
+        
+        buttons.forEach((button, i) => {
+            button.addEventListener("click", () => {
+                items.forEach(item => item.classList.remove("carousel__item--selected"));
+                buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+
+                items[i].classList.add("carousel__item--selected");
+                button.classList.add("carousel__button--selected");
+            })
+        })
+
+        const next = document.getElementById("nextbutton");
+        const prev = document.getElementById("prevbutton");
+
+        console.log(next);
+        next.addEventListener("click", () => {
+            items.forEach(item => item.classList.remove("carousel__item--selected"));
+            buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+
+            console.log("A");
+            if (idx+1 > maks) {
+                idx = 0;
+            } else {
+                idx++;
+            }
+            items[idx].classList.add("carousel__item--selected");
+            buttons[idx].classList.add("carousel__button--selected");
+        })
+        prev.addEventListener("click", () => {
+            items.forEach(item => item.classList.remove("carousel__item--selected"));
+            buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+
+            if (idx-1 < 0) {
+                idx = maks;
+            } else {
+                idx--;
+            }
+            items[idx].classList.add("carousel__item--selected");
+            buttons[idx].classList.add("carousel__button--selected");
+        })
+
+        items[0].classList.add("carousel__item--selected");
+        buttons[0].classList.add("carousel__button--selected");
+    })
 });
